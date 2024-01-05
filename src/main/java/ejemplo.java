5498a709481d13001fffff31e71a67430315b4a3
franklinjav <franklin.oliveros@educa.madrid.org>
2024-01-05 17:45:53 +0100

    cambio 1

--- src/main/java/ejemplo.java
+++ src/main/java/ejemplo.java
@@ -1,5 +1,8 @@
 public class ejemplo {
     public static void main(String[] args) {
         System.out.println("Esto es un ejemplo gitconsola");
+        System.out.println("primer cambio");
     }
+
 }
+
