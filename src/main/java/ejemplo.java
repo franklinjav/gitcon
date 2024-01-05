11dd56848bb454ec6418a13af16dc2eb6fad1db7
franklinoli <f.oliverosafonso2@gmail.com>
2024-01-05 17:54:00 +0100

    fff

--- src/main/java/ejemplo.java
+++ src/main/java/ejemplo.java
@@ -2,6 +2,7 @@ public class ejemplo {
     public static void main(String[] args) {
         System.out.println("Esto es un ejemplo gitconsola");
         System.out.println("primer cambio");
+        System.out.println("Cambio realizado desde otro ordenador");
     }
 
 }
